d009884fa86855db579b3926e49efac4fd6f61e0
Jacob Beers <jacobtbeers@gmail.com>
2024-02-29 15:13:51 -0600

    Add build/install script

---
+++ Run-LocalInstall.ps1
@@ -0,0 +1,15 @@
+npm run build
+
+npm run pack
+
+If ($?) {
+    code --uninstall-extension ortus-solutions.boxlang
+
+    $latestBuild = get-childitem boxlang-*.vsix | Sort-Object -Descending LastWriteTime | Select-Object -First 1
+
+    code --install-extension $latestBuild.Name
+}
+Else {
+    Write-Host "Error occurred while building extension"
+}
+
