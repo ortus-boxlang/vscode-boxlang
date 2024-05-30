d53ec9d868f1b93ac59e49bebaea35b982ad1a42
Jacob Beers <jacobtbeers@gmail.com>
2024-05-30 09:44:26 -0500

    BLIDE-20 Add run file to command palette

--- src/commands/runBoxLangFile.ts
+++ src/commands/runBoxLangFile.ts
@@ -2,26 +2,27 @@ import * as fs from "fs";
 import * as vscode from "vscode";
 
 export async function runBoxLangFile(context: vscode.ExtensionContext, filePath) {
+    const fsPath = determineFile(filePath);
     const debugConfig: vscode.DebugConfiguration = {
         name: "BoxLang",
         type: "boxlang",
         request: "launch",
-        program: filePath.fsPath
+        program: fsPath
     };
 
-    if (filePath.fsPath.endsWith(".bx") && !(await hasMainFunction(filePath.fsPath))) {
+    if (fsPath.endsWith(".bx") && !(await hasMainFunction(fsPath))) {
         vscode.window.showErrorMessage("You must implement a main function in order to run a BoxLang class.");
         return;
     }
 
-    if (filePath.fsPath.endsWith(".bx")) {
+    if (fsPath.endsWith(".bx")) {
         const args = await vscode.window.showInputBox({
             title: "Run BoxLang Class",
             prompt: "Enter the arguments to pass to the main class",
-            value: context.workspaceState.get("runArgs" + filePath.fsPath)
+            value: context.workspaceState.get("runArgs" + fsPath)
         });
 
-        context.workspaceState.update("runArgs" + filePath.fsPath, args);
+        context.workspaceState.update("runArgs" + fsPath, args);
         debugConfig.program += " " + args;
     }
 
@@ -32,4 +33,12 @@ async function hasMainFunction(filePath) {
     const content = await fs.readFileSync(filePath);
 
     return /function\s+main\(/i.test(content + "");
+}
+
+function determineFile(filePath) {
+    if (filePath != null) {
+        return filePath.fsPath;
+    }
+
+    return vscode.window.activeTextEditor.document.uri.fsPath;
 }
